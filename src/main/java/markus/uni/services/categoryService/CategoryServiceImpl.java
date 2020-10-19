1593df3f2f0336480863af4584b6e2c32df5c90f
pr0gr4mm4r <45204482+pr0gr4mm4r@users.noreply.github.com>
2020-10-19 10:54:25 +0200

    small changes

--- src/main/java/markus/uni/services/categoryService/CategoryServiceImpl.java
+++ src/main/java/markus/uni/services/categoryService/CategoryServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
