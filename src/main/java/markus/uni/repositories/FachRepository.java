3400a66a63754e7900f6019ffd52a1873af1e664
pr0gr4mm4r <45204482+pr0gr4mm4r@users.noreply.github.com>
2020-10-19 11:25:10 +0200

    universal Repo change

--- src/main/java/markus/uni/repositories/FachRepository.java
+++ src/main/java/markus/uni/repositories/FachRepository.java
@@ -6,16 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.List;
-import java.util.Optional;
-
 @CrossOrigin
 @Repository
 public interface FachRepository extends JpaRepository<Fach, Integer> {
     @Query(value = "select * from fach where fach_name = :fachName" , nativeQuery = true)
     Fach getByFachName(String fachName);
-    @Query(value = "select max(fach_id) from fach where fach_name like %:shortenedFachName%" , nativeQuery = true)
-    Optional<Integer> getFachIdForSort(String shortenedFachName);
-    @Query(value = "select * from fach where fach_id > :fachId" , nativeQuery = true)
-    List<Fach> getAllWhichAreAffected(Integer fachId);
 }
